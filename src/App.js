8baa4a64320345853a89062d746288d802fb38f6
tonnymuchui <tonykanyingah@gmail.com>
2021-04-03 13:20:56 +0300

    menu-item router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import HomePage from './homepage-component/homepage.component';
+import HomePage from './pages/homepage-component/homepage.component';
+import {Switch, Route} from 'react-router-dom';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 function App() {
   return (
     <div className="App">
-      < HomePage />
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
